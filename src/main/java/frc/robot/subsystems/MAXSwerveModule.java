997f4e4c36df1e99922b05efe327b3029e4998b7
aidan <arfiedler1114@gmail.com>
2024-12-17 17:52:14 -0600

    add phoenix lib, make some changes

--- src/main/java/frc/robot/subsystems/MAXSwerveModule.java
+++ src/main/java/frc/robot/subsystems/MAXSwerveModule.java
@@ -11,9 +11,11 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
+import frc.robot.Constants.DriveConstants;
 
 import frc.robot.Configs;
 
@@ -21,8 +23,10 @@ public class MAXSwerveModule {
   private final CANSparkMax m_drivingSpark;
   private final CANSparkMax m_turningSpark;
 
+  public final CANCoder m_absoluteEncoder;  
+
   private final RelativeEncoder m_drivingEncoder;
-  private final AbsoluteEncoder m_turningEncoder;
+  private final RelativeEncoder m_turningEncoder;
 
   private final SparkPIDController m_drivingClosedLoopController;
   private final SparkPIDController m_turningClosedLoopController;
@@ -36,12 +40,13 @@ public class MAXSwerveModule {
    * MAXSwerve Module built with NEOs, SPARKS MAX, and a Through Bore
    * Encoder.
    */
-  public MAXSwerveModule(int drivingCANId, int turningCANId, double chassisAngularOffset) {
-    m_drivingSpark = new CANSparkMax(drivingCANId, MotorType.kBrushless);
-    m_turningSpark = new CANSparkMax(turningCANId, MotorType.kBrushless);
-
-    m_drivingEncoder = m_drivingSpark.getEncoder();
-    m_turningEncoder = m_turningSpark.getAbsoluteEncoder();
+  public MAXSwerveModule(
+    int driveMotorChannel,
+    int turningMotorChannel,
+    int turningEncoderChannel,
+    double offset) {
+    m_drivingSpark = new CANSparkMax(driveMotorChannel, MotorType.kBrushless);
+    m_turningSpark = new CANSparkMax(turningMotorChannel, MotorType.kBrushless);
 
     m_drivingClosedLoopController = m_drivingSpark.getPIDController();
     m_turningClosedLoopController = m_turningSpark.getPIDController();
@@ -49,14 +54,41 @@ public class MAXSwerveModule {
     // Apply the respective configurations to the SPARKS. Reset parameters before
     // applying the configuration to bring the SPARK to a known good state. Persist
     // the settings to the SPARK to avoid losing them on a power cycle.
-    m_drivingSpark.configure(Configs.MAXSwerveModule.drivingConfig, ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
-    m_turningSpark.configure(Configs.MAXSwerveModule.turningConfig, ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
+     m_drivingSpark = new CANSparkMax(driveMotorChannel, MotorType.kBrushless);
+     m_turningSpark = new CANSparkMax(turningMotorChannel, MotorType.kBrushless);
+     m_drivingSpark.setIdleMode(IdleMode.kBrake);
+     m_turningSpark.setInverted(false);
+     m_turningSpark.burnFlash();
+     m_drivingSpark.burnFlash();
+ 
+     m_drivingEncoder = m_drivingSpark.getEncoder();
+     m_drivingEncoder.setPosition(0);
+     m_turningEncoder = m_turningSpark.getEncoder();
+     m_absoluteEncoder = new CANCoder(turningEncoderChannel);
+ 
+     // Configuration settings for Absolute Encoder
+     CANCoderConfiguration config = new CANCoderConfiguration();
+     config.sensorCoefficient = 2 * Math.PI / kEncoderResolution;
+     config.unitString = "rad";
+     config.sensorTimeBase = SensorTimeBase.PerSecond;
+     config.magnetOffsetDegrees = 0;
+     config.absoluteSensorRange = AbsoluteSensorRange.Signed_PlusMinus180;
 
-    m_chassisAngularOffset = chassisAngularOffset;
-    m_desiredState.angle = new Rotation2d(m_turningEncoder.getPosition());
-    m_drivingEncoder.setPosition(0);
+     config.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+     m_absoluteEncoder.configAllSettings(config);
+ 
+     m_driveEncoder.setPositionConversionFactor(2 * Math.PI * kWheelRadius / kEncoderResolution);
+     m_turningEncoder.setPositionConversionFactor(12.8 * Math.PI * 2);
+ 
+     // Limit the PID Controller's input range between -pi and pi and set the input
+     // to be continuous.
+     m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+ 
+     absoluteEncoderOffset = offset;
+ 
+     m_chassisAngularOffset = chassisAngularOffset;
+     m_desiredState.angle = new Rotation2d(m_turningEncoder.getPosition());
+     m_drivingEncoder.setPosition(0);
   }
 
   /**
